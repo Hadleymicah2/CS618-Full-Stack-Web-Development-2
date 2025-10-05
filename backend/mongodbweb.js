65aece3d800741359aef80153675bb71d0870233
Hadleymicah <Hadleyfig@gmail.com>
2025-10-05 17:30:39 +0000

    chore: set up mongodb

---
+++ backend/mongodbweb.js
@@ -0,0 +1,33 @@
+import { createServer } from 'node:http';
+import { MongoClient } from 'mongodb';
+
+//connecting to a mongo database
+const url = 'mongodb://localhost:27017/';
+const dbName = 'mydb';
+const client = new MongoClient(url);
+
+try {
+  await client.connect();
+  console.log('Sucessfully connected to database!');
+} catch (err) {
+  console.error('Error connecting to database:', err);
+}
+
+// creating a server
+const server = createServer(async (req, res) => {
+  // when someone requests something from the server (what is going to be executed)
+  const db = client.db(dbName);
+  const users = db.collection('users');
+  const usersList = await users.find().toArray();
+
+  res.ststusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify(usersList));
+});
+
+//runnihg a server
+const host = 'localhost';
+const port = 3000;
+server.listen(port, host, () => {
+  console.log(`Server listening on http://${host}:${port}`);
+});
