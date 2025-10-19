b7da17407e22ee6ed1d807aae6d6dda7614c38b2
Hadleymicah <Hadleyfig@gmail.com>
2025-10-19 20:56:57 +0000

    chore: recreated fetch blog posts

---
+++ src/api/posts.js
@@ -0,0 +1,10 @@
+// For communicating about posts
+//Creating a function for getting posts from a server by using a fetch method
+
+export const getPosts = async (queryParams) => {
+  const res = await fetch(
+    `${import.meta.env.VITE_BACKEND_URL}/posts?` +
+      new URLSearchParams(queryParams),
+  )
+  return await res.json()
+}
